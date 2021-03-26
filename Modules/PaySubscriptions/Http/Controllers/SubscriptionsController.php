bb047de8d31b0c1d8909c06b673da298287406d3
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-03-26 09:54:17 -0600

    Store in module subscriptions

--- Modules/PaySubscriptions/Http/Controllers/SubscriptionsController.php
+++ Modules/PaySubscriptions/Http/Controllers/SubscriptionsController.php
@@ -2,9 +2,14 @@
 
 namespace Modules\PaySubscriptions\Http\Controllers;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use Modules\PaySubscriptions\Entities\Package;
+use Modules\PaySubscriptions\Entities\Subscription;
+use Modules\PaySubscriptions\Http\Requests\SubscriptionRequest;
 
 class SubscriptionsController extends Controller
 {
@@ -14,7 +19,8 @@ class SubscriptionsController extends Controller
      */
     public function index()
     {
-        return view('paysubscriptions::subscriptions.index');
+        $subscriptions = Subscription::paginate();
+        return view('paysubscriptions::subscriptions.index', compact('subscriptions'));
     }
 
     /**
@@ -23,6 +29,7 @@ class SubscriptionsController extends Controller
      */
     public function create()
     {
+
         return view('paysubscriptions::subscriptions.create');
     }
 
@@ -31,9 +38,39 @@ class SubscriptionsController extends Controller
      * @param Request $request
      * @return Response
      */
-    public function store(Request $request)
+    public function store(SubscriptionRequest $request)
     {
-        //
+        try {
+            $package = Package::where('id', $request->package_id)->first();
+            $dt = Carbon::create($request->start_date);
+            $trial_end_date = $package->trial_days > 0 ? $dt->addDays($package->trial_days) : null;
+            
+            if ($package->interval == 'days') {
+                $end_date = $dt->addDays($package->interval_count);
+            } elseif ($package->interval == 'months') {
+                $end_date = $dt->addMonths($package->interval_count);
+            } else {
+                $end_date = $dt->addYears($package->interval_count);
+            }
+            
+            Subscription::create([
+                'user_id' => $request->user_id,
+                'package_id' => $request->package_id,
+                'status' => $request->status,
+                'start_date' => $request->start_date,
+                'trial_end_date' => $trial_end_date,
+                'end_date' => $end_date,
+                'created_id' => auth()->user()->id,
+                'package_price' => $package->price,
+                'package_details' => $package->description,
+                'paid_via' => 'offline',
+                'payment_transaction_id' => null,
+            ]);
+            
+            return redirect()->back()->with('success', __('paysubscriptions::global.successfully_added'));
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('danger', "Error: " . $th->getMessage());
+        }
     }
 
     /**
@@ -75,5 +112,30 @@ class SubscriptionsController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function getUser(Request $request) {        
+        $search = $request->search;
+        $users = [];
+        
+        if($search == ''){
+            $users = User::orderby('name','asc')->select('id','name')->limit(5)->get();
+        }else{
+            $users = User::orderby('name','asc')->select('id','name')->where('name', 'like', '%' .$search . '%')->limit(10)->get();
+        }
+        
+        return response()->json($users);
+    }
+
+    public function getPackage(Request $request) {        
+        $search = $request->search;
+        $packages = [];
+        
+        if($search == ''){
+            $packages = Package::orderby('name','asc')->select('id','name')->where('is_active', 1)->limit(5)->get();
+        }else{
+            $packages = Package::orderby('name','asc')->select('id','name')->where('name', 'like', '%' .$search . '%')->where('is_active', 1)->limit(10)->get();
+        }
+        return response()->json($packages);
     }
 }
