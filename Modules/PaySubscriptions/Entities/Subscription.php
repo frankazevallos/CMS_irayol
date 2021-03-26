d2cdea96d04bcc051154c6f1740acf477d571b5a
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-03-26 09:52:06 -0600

    Asignacion masiva

--- Modules/PaySubscriptions/Entities/Subscription.php
+++ Modules/PaySubscriptions/Entities/Subscription.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
-    protected $fillable = [];
+    protected $fillable = [
+        'user_id',
+        'package_id',
+        'status',
+        'start_date',
+        'trial_end_date',
+        'end_date',
+        'created_id',
+        'package_price',
+        'package_details',
+        'paid_via',
+        'payment_transaction_id',
+    ];
 }
