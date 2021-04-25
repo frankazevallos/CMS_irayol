bcdb3338d35d1e9315a61a8cbd818eb283fa3896
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-04-25 13:21:15 -0600

    Mejoras al administrador de archivos

--- resources/views/media/items.blade.php
+++ resources/views/media/items.blade.php
@@ -1,17 +1,23 @@
-<div class="card-body">
-    <div class="row ajaxMediaShow">
-        @foreach($medias as $media)
-            <div class="col-md-2 mt-3" id="card_media_{{ $media->id }}">
-                <div class="card-body img-card-background loading filter image" style="background-image: url('{{ $media->getFile() }}'); ">
-                    <div class="card-tools">
-                        <a href="javascript:void(0)" id="showMedia" data-id="{{ $media->id }}" class="btn btn-tool"><i class="fas fa-eye"></i></a>
+@if (count($medias) > 0)
+    <div class="card-body">
+        <div class="row ajaxMediaShow">
+            @foreach($medias as $media)
+                <div class="col-md-2 mt-3" id="card_media_{{ $media->id }}">
+                    <div class="card-body img-card-background loading filter image" style="background-image: url('{{ $media->getFile() }}'); ">
+                        <div class="card-tools">
+                            <a href="javascript:void(0)" id="showMedia" data-id="{{ $media->id }}" class="btn btn-tool"><i class="fas fa-eye"></i></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
-</div>
 
-<div class="card-footer">
-    {!! $medias->render() !!}
-</div>
+    <div class="card-footer">
+        {!! $medias->render() !!}
+    </div>
+@else
+    <div class="alert alert-warning text-center m-3">
+        <h4 class="alert-heading">{{__('global.no_results')}}</h4>
+    </div>
+@endif
