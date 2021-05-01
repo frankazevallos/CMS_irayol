b1de0633d92a3de2e662b0f604a3dd053bfc0eeb
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-05-01 06:49:35 -0600

    Se removio codigo imnecesario

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -21,34 +21,6 @@ class Media extends Model
         'extension'
     ];
 
-    /**
-     * @var array
-     */
-    public function saveFile($files){
-
-        $destinationPath = '/uploads/' . date('Y') . '/' . date('m') . '/' . date('d'); //chmod 0777
-
-        $data = [];
-
-        foreach ($files as $file) {
-            $filename = $file->getClientOriginalName();
-            $extension = $file->getClientOriginalExtension();
-
-            $dataMedia = $this->create([
-                'user_id' => auth()->user()->id,
-                'file' => $filename,
-                'path' => $destinationPath . '/' . $filename,
-                'extension' => $extension,
-            ]);
-
-            $data[] = array('id' => $dataMedia->id, 'path' => $dataMedia->getFile());
-
-            $file->storeAs($destinationPath, $filename, 'public');
-        }
-
-        return $data;
-    }
-
     public function getFile()
     {
         $image = ['gif', 'png', 'jpg', 'jpeg', 'raw', 'webp',];
