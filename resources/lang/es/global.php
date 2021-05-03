5872bca333c1a70b1622648b2bb5cbdaf2b4c62c
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-05-02 20:57:44 -0600

    Correcciones en ES

--- resources/lang/es/global.php
+++ resources/lang/es/global.php
@@ -42,6 +42,7 @@ return [
 	],
 
 	'create' => 'Nuevo',
+	'new' => 'Nuevo',
 	'save' => 'Guardar',
 	'cancel' => 'Cancelar',
 	'close' => 'Cerrar',
@@ -56,7 +57,7 @@ return [
 	'search' => 'Buscar',
 	'upload' => 'Subir',
 	'return_back' => 'Volver atrás',
-	'number_of_items' => 'Numero de resultados',
+	'number_of_items' => 'Número de resultados',
 	'route' => 'Ruta',
 	'size' => 'Tamaño en disco',
     'email' => 'Correo',
@@ -76,9 +77,13 @@ return [
     'new_password' => 'Nueva contraseña',
     'new_confirm_password' => 'Confirmar nueva contraseña',
 
+	'published' => 'Publicado',
+	'draft' => 'Borrador',
+	'pending_review' => 'Pendiente de revisión',
+
     'url_website' => 'Sitio web',
     'url_facebook' => 'Facebook',
-    'url_twitter' => 'Twitetr',
+    'url_twitter' => 'Twitter',
     'url_instagram' => 'Instagram',
     'url_linkedin' => 'LinkedIn',
     'url_github' => 'GitHub',
@@ -92,19 +97,19 @@ return [
     'delete_account' => 'Borrar mi cuenta',
     'posted_by' => 'Publicado por',
 
-	'seo_title' => 'Titulo para SEO',
-	'seo_description' => 'Descripciòn para SEO',
-	'seo_keyword' => 'Palabras para SEO',
+	'seo_title' => 'Título para SEO',
+	'seo_description' => 'Descripción para SEO',
+	'seo_keyword' => 'Palabras clave para SEO',
 
-	'app_no_entries_in_table' => 'No entries in table',
-	'custom_controller_index' => 'Custom controller index.',
-	'app_logout' => 'Logout',
+	'app_no_entries_in_table' => 'No hay entradas en la tabla',
+	'custom_controller_index' => 'Índice de controlador personalizado',
+	'app_logout' => 'Cerrar sesión',
 	'add_new' => 'Agregar nuevo',
 	'add' => 'Agregar',
 	'create_new' => 'Crear nueva',
-	'are_you_sure' => '¿Estas seguro?',
+	'are_you_sure' => '¿Estás seguro?',
 	'back_to_list' => 'Volver a la lista',
-	'dashboard' => 'Inicio',
+	'dashboard' => 'Tablero',
 	'global_title' => 'Administrar Roles y Permisos',
 
 	// Admin Home
@@ -128,17 +133,17 @@ return [
 	'profile' => 'Perfil',
 	'settings' => 'Configuración',
 	'themes' => 'Temas',
-	'modules' => 'Modulos',
+	'modules' => 'Módulos',
 	'menus' => 'Menús',
 	'menu' => 'Menú',
 
 	// Card header
-	'list_page' => 'Lista de paginas',
+	'list_page' => 'Lista de páginas',
 	'list_blogs' => 'Lista de entradas',
 	'list_categories' => 'Lista de categorías',
 	'list_media' => 'Lista multimedia',
 	'list_themes' => 'Lista de temas',
-	'list_modules' => 'Lista de modulos',
+	'list_modules' => 'Lista de módulos',
 	'list_menus' => 'Lista de menús',
 	'list_usuarios' => 'Lista de usuarios',
 
@@ -146,7 +151,7 @@ return [
 	'title' => 'Título',
 	'url' => 'URL',
 	'author' => 'Autor',
-	'update_at' => 'Ultima actualización',
+	'update_at' => 'Última actualización',
 	'created_at' => 'Fecha creación',
 	'action' => 'Acción',
 	'keyword' => 'Palabras clave',
@@ -156,7 +161,7 @@ return [
 
 	// Form
 	'apply' => 'Aplicar',
-	'number_of_items' => 'Numero de registros',
+	'number_of_items' => 'Número de registros',
 	'status' => 'Estado',
 	'active' => 'Activo',
 	'inactive' => 'Inactivo',
@@ -176,7 +181,7 @@ return [
 	'menu_structure' => 'Estructura del menú',
 	'menu_indication' => 'Ponga cada artículo en el orden que prefiera. Haga clic en la flecha a la derecha del artículo para mostrar más opciones de configuración.',
 	'custom_link' => 'Enlace personalizado',
-	'subelement' => 'Subelemento',
+	'subelement' => 'Sub elemento',
 	'class_CSS' => 'Clases CSS',
 	'optional' => 'Opcional',
 
@@ -192,7 +197,7 @@ return [
 	'successfully_updated' => 'Actualizado exitosamente',
 	'successfully_deleted' => 'Eliminado con éxito',
 	'not_found' => 'No encontrado',
-	'are_you_sure?' => '¿Estas seguro?',
+	'are_you_sure?' => '¿Estás seguro?',
 	'it_will_be_deleted_permanently' => '¡Será eliminado permanentemente!',
 	'something_went_wrong_with_ajax' => 'Algo salió mal con ajax',
 
@@ -200,11 +205,11 @@ return [
     'successfully_updated' => 'El registro fue actualizado con éxito.',
     'successfully_destroy' => 'El registro fue eliminado con éxito.',
 
-	'successfully_added_addons' => 'El modulo :module fue agregado con éxito.',
-	'successfully_destroy_addons' => 'El modulo :module fue eliminado con éxito.',
+	'successfully_added_addons' => 'El módulo :module fue agregado con éxito.',
+	'successfully_destroy_addons' => 'El módulo :module fue eliminado con éxito.',
 
-	'successfully_added_theme' => 'El modulo :theme fue agregado con éxito.',
-	'successfully_destroy_theme' => 'El modulo :theme fue eliminado con éxito.',
+	'successfully_added_theme' => 'El módulo :theme fue agregado con éxito.',
+	'successfully_destroy_theme' => 'El módulo :theme fue eliminado con éxito.',
 	'main_page' => 'Página principal',
 	'main_menu' => 'Menú principal',
 	'insert' => 'Insertar',
