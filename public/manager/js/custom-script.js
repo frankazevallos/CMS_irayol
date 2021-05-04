2ed04bf5fc3feced8450975c961a91a19eabf7e6
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-05-03 22:18:07 -0600

    Toast globales

--- public/manager/js/custom-script.js
+++ public/manager/js/custom-script.js
@@ -21,6 +21,18 @@ $('.datetimepicker').datetimepicker({
     icons: icons,
 });
 
+const Toast = Swal.mixin({
+    toast: true,
+    position: "bottom-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+});
+
 /* CLOSE GLOBAL CONST */
 
 //Close alert
