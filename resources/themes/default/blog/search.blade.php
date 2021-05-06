3e4daa43ab27a0786215e578b40de7415805108c
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-05-05 22:20:05 -0600

    Mejoras a la indentaciòn

--- resources/themes/default/blog/search.blade.php
+++ resources/themes/default/blog/search.blade.php
@@ -11,26 +11,23 @@
         @foreach ($blogs as $blog)
         <div class="col mt-4">
             <div class="card h-100">
-                <a href="{{route('blog.show', $blog->slug)}}"><img
-                        src="{{$blog->main_image ? $blog->main_image : asset('manager/images/placeholder-image.jpg')}}"
-                        class="card-img-top"></a>
+                <a href="{{route('blog.show', $blog->slug)}}"><img src="{{$blog->main_image ? $blog->main_image : asset('manager/images/placeholder-image.jpg')}}" class="card-img-top"></a>
                 <div class="card-body">
                     <h5 class="card-title"><a href="{{route('blog.show', $blog->slug)}}">{{$blog->title}}</a></h5>
                     @if (strlen($blog->content) > 150)
-                    {{substr($blog->content, 0, 150, ) . '...'}}
+                        {!! substr($blog->content, 0, 150, ) . '...' !!}
                     @else
-                    {{$blog->content}}
+                        {!! $blog->content !!}
                     @endif
                 </div>
                 <div class="card-footer">
-                    <i class="fas fa-user-circle"></i> {{__('global.posted_by')}} <a
-                        href="{{route('users.show', $blog->user->id)}}">{{ $blog->user->name}}</a><br>
+                    <i class="fas fa-user-circle"></i> {{__('global.posted_by')}} <a href="{{route('users.show', $blog->user->id)}}">{{ $blog->user->name}}</a><br>
                     <i class="fas fa-clock"></i> {{$blog->created_at->format('Y-m-d')}}<br>
                     @if (count($blog->categories) > 0)
-                    <i class="fas fa-tag"></i>
-                    @foreach ($blog->categories as $category)
-                    <a href="{{ route('site.category', $category->slug) }}">{{$category->name}}</a>
-                    @endforeach
+                        <i class="fas fa-tag"></i>
+                        @foreach ($blog->categories as $category)
+                            <a href="{{ route('site.category', $category->slug) }}">{{$category->name}}</a>
+                        @endforeach
                     @endif
                 </div>
             </div>
