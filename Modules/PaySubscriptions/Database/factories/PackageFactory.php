8ac8b39cd4245b760e625bb87882c6426c2e60a9
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-05-15 22:36:29 -0600

    Mejoras a los datos de prueba

--- Modules/PaySubscriptions/Database/factories/PackageFactory.php
+++ Modules/PaySubscriptions/Database/factories/PackageFactory.php
@@ -24,8 +24,6 @@ class PackageFactory extends Factory
             'user_id' => $this->faker->numberBetween($min = 1, $max = 11),
             'name' => $this->faker->sentence($nbWords = 6, $variableNbWords = true),
             'description' => $this->faker->text($maxNbChars = 500),
-            'interval' => $this->faker->randomElement(['days', 'weeks', 'moths', 'years']),
-            'interval_count' => 1,
             'trial_days' => $this->faker->numberBetween($min = 0, $max = 10),
             'price' => $this->faker->numberBetween($min = 10, $max = 25),
             'is_active' => 1
