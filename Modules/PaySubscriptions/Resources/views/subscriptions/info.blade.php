6aece4181aae1195f72bfdcf3f75f255ed2469cb
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-05-17 22:54:37 -0600

    Mejoras en vistas para procesar pagos

--- Modules/PaySubscriptions/Resources/views/subscriptions/info.blade.php
+++ Modules/PaySubscriptions/Resources/views/subscriptions/info.blade.php
@@ -1,29 +1,35 @@
 @extends('layouts.frontend')
 @push('title', __('paysubscriptions::global.pay_subscriptions'))
 @section('content')
-<div class="container my-3">
-    <div class="row row-cols-1 row-cols-md-3 text-center">
-        @foreach ($packages as $package)
-        <div class="col mt-3">
-            <div class="card h-100 shadow">
-                <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal">{{$package->name}}</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">{{ '$' . number_format($package->price, 2) }}<small class="text-muted fw-light">/{{ __('paysubscriptions::global.interval.'.$package->interval) }}</small></h1>
-                    <p>
-                        {!! $package->description !!}
-                    </p>
-                </div>
-                <div class="card-footer">
-                    <form action="{{route('pay.subscription', $package->id)}}" method="post">
-                        @csrf
-                        <button type="submit" class="w-100 btn btn-lg btn-outline-primary">{{__('paysubscriptions::global.become_premium')}}</button>
-                    </form>
+    <div class="container my-5">
+        <div class="text-center">
+            <h1 class="display-4">{{__('paysubscriptions::global.invest_in_yourself')}}</h1>
+            <p class="lead">{{__('paysubscriptions::global.with_any_subscription')}}</p>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 text-center">
+            @foreach ($packages as $package)
+            <div class="col mt-3">
+                <div class="card h-100 shadow">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal">{{$package->name}}</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">{{ '$' . number_format($package->price, 2) }}<small
+                            class="text-muted fw-light">/{{ __('paysubscriptions::global.interval.'.$package->interval) }}</small>
+                        </h1>
+                        <p>
+                            {!! $package->description !!}
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="{{route('subscription.index', $package->id)}}" class="w-100 btn btn-lg btn-outline-primary">{{__('paysubscriptions::global.become_premium')}} <i class="fas fa-arrow-right"></i></a>
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
+        <div class="col-md-12 mt-4">
+            {{$packages->links()}}
+        </div>
     </div>
-</div>
 @endsection
---
+++ Modules/PaySubscriptions/Resources/views/subscriptions/pay.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.frontend')
+@push('title', __('paysubscriptions::global.pay_subscriptions'))
+@section('content')
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <strong>{{__('paysubscriptions::global.package')}}</strong>
+                        <h4>{{ $package->name}}</h4>
+                        <hr>
+                        
+                        <strong>{{__('paysubscriptions::global.interval.interval_count')}}</strong>
+                        <h4>{{ __('paysubscriptions::global.interval.'.$package->interval) }}</h4>
+                        <hr>
+                        
+                        <strong>{{__('paysubscriptions::global.trial_days')}}</strong>
+                        <h4>{{ $package->trial_days }}</h4>
+                        <hr>
+                        
+                        <strong>{{__('paysubscriptions::global.price')}}</strong>
+                        <h4>{{ '$' . number_format($package->price, 2) }}</h4>
+                        <hr>
+                        
+                        <strong>{{__('paysubscriptions::global.description')}}</strong>
+                        <p>{!! $package->description !!}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <form action="" method="post">
+                    @csrf
+                    <div class="card">
+                        <div class="card-header">
+                            {{__('paysubscriptions::global.invest_in_yourself')}}
+                        </div>
+                        <div class="card-body">
+                                <label>Select the desired payment platform:</label>
+                                <div class="form-group" id="toggler">
+                                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                        @foreach (setting('paymentPlatforms') as $paymentPlatform)
+                                        <label class="btn btn-outline-secondary rounded m-2 p-1" data-target="#{{ $paymentPlatform }}Collapse" data-toggle="collapse">
+                                            <input type="radio" name="payment_platform" value="{{ $paymentPlatform }}" required>
+                                            <img class="img-fluid" src="{{ asset('modules/paysubscriptions/img/' . $paymentPlatform . '.jpg') }}">
+                                        </label>
+                                        @endforeach
+                                    </div>
+                                    @foreach (setting('paymentPlatforms') as $paymentPlatform)
+                                        <div id="{{ $paymentPlatform }}Collapse" class="collapse" data-parent="#toggler">
+                                            @includeIf('paysubscriptions::components.' . strtolower($paymentPlatform) . '-collapse')
+                                        </div>
+                                    @endforeach
+                                </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
